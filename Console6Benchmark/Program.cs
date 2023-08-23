d8e02904df413ac38900997d8012703572b9b316
Diego Andres Yave Guzmain <di.an.yave@gmail.com>
2023-08-24 00:47:04 +0200

    Add benchmark example

--- Console6Benchmark/Program.cs
+++ Console6Benchmark/Program.cs
@@ -1,9 +1,34 @@
-﻿namespace Console6Benchmark;
+﻿using System.Security.Cryptography;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Console6Benchmark;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+        Console.WriteLine("This is the end.");
     }
 }
 
+public class Md5VsSha256
+{
+    private const int N = 10000;
+    private readonly byte[] data;
+
+    private readonly SHA256 sha256 = SHA256.Create();
+    private readonly MD5 md5 = MD5.Create();
+
+    public Md5VsSha256()
+    {
+        data = new byte[N];
+        new Random(42).NextBytes(data);
+    }
+
+    [Benchmark]
+    public byte[] Sha256() => sha256.ComputeHash(data);
+
+    [Benchmark]
+    public byte[] Md5() => md5.ComputeHash(data);
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Console App with BenchmarkDotNet
+
+Run as release:
+
+dotnet run -p Console6Benchmark.csproj -c Release
